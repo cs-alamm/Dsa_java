69ae51beedf43bcc19f6e3e67e8f5f584bdef157
cs-alamm <ma9788181@gmail.com>
2025-07-14 11:38:16 +0530

    add reverse problem in an array

---
+++ reverse.java
@@ -0,0 +1,26 @@
+public class reverse {
+    
+
+    public static void reverseArr(int number[]){
+        int first = 0; int last = number.length-1;
+
+        while(first < last){
+            int temp = number[first];
+            number[first] = number[last];
+            number[last] = temp;
+            first++;
+            last--;
+           
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        int number[] = {2, 4, 5, 8, 9, 7};
+         reverseArr(number);
+         for(int i = 0; i<number.length; i++){
+            System.out.print(number[i] +"  ");
+         }
+        //  System.out.println();
+    }
+}
